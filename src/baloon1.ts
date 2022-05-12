a06ffd3ba12ab1f0aff872fedd44434ef5736656
y-omasa-kbc <y-omasa@kawahara.ac.jp>
2022-05-13 00:18:39 +0900

    add: Step1 provided source
    Step 1:  風船が一つ。不自然な速度で右移動するので、速度を調整する

--- src/baloon1.ts
+++ src/baloon1.ts
@@ -1,5 +1,6 @@
 import { ParentBaloon } from './fixed/ParentBaloon';
 
+//風船の設計図
 export class Baloon1 extends ParentBaloon {
   img: HTMLImageElement;
   
@@ -7,6 +8,7 @@ export class Baloon1 extends ParentBaloon {
   imageX: number; //風船の位置のx座標
   imageY: number; //風船の位置のy座標
 
+  // ↓最初に風船を作り出すときの処理
   constructor(img: HTMLImageElement) {
     super();
     this.img = img;
@@ -16,10 +18,12 @@ export class Baloon1 extends ParentBaloon {
     this.imageY = 100;  //記憶のimageYを100にする
   }
 
+  // ↓フレームが進むたびに風船に起きる変化
   move() {
     this.imageX = this.imageX + 10;
   }
 
+  // ↓フレームで風船を描画（今回は変更しない）
   draw(cntx: CanvasRenderingContext2D ): void {
     cntx.drawImage(this.img, this.imageX, this.imageY); 
   }
